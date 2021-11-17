27e616b9096e4e67478c90d1fe723503c84987d7
Timowest12 <timowester@gmail.com>
2021-11-17 09:54:59 +0100

    Add getlikes function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
-import { getmovielist, getfromid,openModal } from './MoviesAPI.js'
+import { getmovielist, getfromid,openModal, updatelikes, postlike } from './MoviesAPI.js'
 import { getComments, postComment, getmovie } from './popup.js'
 
+postlike('1868450')
 getmovielist();
 getmovie();
