140c8e946d885e5e76096fd0e30091b5589cc48b
Timowest12 <timowester@gmail.com>
2021-11-17 10:37:03 +0100

    Fix auto update issue

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './style.css';
 import { getmovielist, getfromid,openModal, updatelikes, postlike } from './MoviesAPI.js'
 import { getComments, postComment, getmovie } from './popup.js'
 
-postlike('1868450')
+
 getmovielist();
 getmovie();
